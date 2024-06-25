f266757d362e2d3eeb726a151af18eebe12016e6
Aleksey767 <al_sazonov@list.ru>
2024-06-25 16:41:51 +0200

    add remove by ID, get by ID

--- src/main/java/BustTicketService.java
+++ src/main/java/BustTicketService.java
@@ -3,19 +3,44 @@ import java.util.ArrayList;
 public class BustTicketService {
 
     private static ArrayList<BusTicket> tickets;
+
     public BustTicketService() {
         tickets = new ArrayList<>();
     }
+
     public void createTicket() {
         BusTicket ticket = new BusTicket();
         tickets.add(ticket);
     }
-    public void createTicket(String startDate,String ticketType) {
+
+    public void createTicket(String startDate, String ticketType) {
         BusTicket ticket = new BusTicket(startDate, ticketType);
         tickets.add(ticket);
     }
-    public void createTicket(int id,String ticketClass,String ticketType,String startDate,int price) {
-        BusTicket ticket = new BusTicket(id,ticketClass,ticketType,startDate,price);
+
+    public void createTicket(int id, String ticketClass, String ticketType, String startDate, int price) {
+        BusTicket ticket = new BusTicket(id, ticketClass, ticketType, startDate, price);
         tickets.add(ticket);
+    }
+
+    public boolean removeById(int id) {
+        for (BusTicket ticket : tickets) {
+            if (ticket.getId() == id) {
+                tickets.remove(ticket);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public BusTicket getById(int id) {
+        if (id >= 0 && id <= tickets.size()) {
+            for (BusTicket ticket : tickets) {
+                if (ticket.getId() == id) {
+                    return ticket;
+                }
+            }
+        }
+        return null;
     }
 }
